85d098ec6d94e84f519a0e5abdb1e451e87486c8
arnaudgregoire <arnaud.gregoire@ensg.eu>
2018-04-25 20:56:57 +0200

    premier terrain, première grille, premier structure json

--- client/js/main.js
+++ client/js/main.js
@@ -6,12 +6,56 @@ var ctx = canvas.getContext("2d");
 
 document.getElementById('buttonPlanter').addEventListener("click", planter);
 
-function planter() {
-    fetch("http://localhost:8080/").then(function(response){
+
+class Terrain{
+    constructor(nbHauteur, nbLargeur){
+        this.nbHauteur = nbHauteur;
+        this.nbLargeur = nbLargeur;
+        this.drawlines();
+        this.update();
+    }
+    drawlines(){
+        for (let x = 0; x < this.nbHauteur; x++) {
+            ctx.beginPath();
+            ctx.moveTo(x*canvas.width/this.nbHauteur,0);
+            ctx.lineTo(x*canvas.width/this.nbHauteur,canvas.height);
+            ctx.stroke();
+            ctx.closePath();
+        }
+    }
+    update(){
+
+    }
+}
+
+let test = new Terrain(10,10);
+
+function send(data) {
+    return fetch("http://localhost:8080/",{
+        method: "POST",
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+        },
+        body: data  
+    })
+}
+
+function planter(x,y) {
+    send(JSON.stringify(
+        {
+            "fonction": "planter",
+            "param": {
+                "x": x,
+                "y": y
+            }
+        }
+    )).then(function(response){
         if(response.ok) {
             response.json().then(function(json) {
+                console.log(json);
                 if(json.reponse == 1){
-                    ctx.fillRect(20,20,150,100); 
+                    ctx.fillRect(x,y,100,100); 
                 }
             });
         }} 
--- serveur/mainserver.js
+++ serveur/mainserver.js
@@ -1,13 +1,19 @@
 const express = require('express')
 var cors = require('cors')
+const bodyParser = require("body-parser");
+
 const app = express()
 
 app.use(cors())
+app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
+app.post('/', function (req, res) {
+  console.log(req.body);
+  let fonction = req.body.fonction;
+  console.log(fonction);
   res.json({"reponse": 1})
 })
 
 app.listen(8080, function () {
-  console.log('listening on port 8080!')
+  console.log('listening on port 8080!');
 })
