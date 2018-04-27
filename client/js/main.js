cd6addddff82fc09a8ffa89a305c6a02594a1387
arnaudgregoire <arnaud.gregoire@ensg.eu>
2018-04-28 01:37:07 +0200

    passage version tableau

--- client/js/main.js
+++ client/js/main.js
@@ -1,34 +1,26 @@
 
-var canvas = document.getElementById('canvas');
-canvas.width = (window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth) /2;
-canvas.height = (window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight) /2;
-var ctx = canvas.getContext("2d");
-
-
-document.getElementById('buttonPlanter').addEventListener("click", planter);
 
 
 class Terrain{
     constructor(nbHauteur, nbLargeur){
         this.nbHauteur = nbHauteur;
         this.nbLargeur = nbLargeur;
-        this.drawlines();
+        this.cases = new Array(nbHauteur);
+        // Initialisation de la matrice contenant les cases
+        for (var i = 0; i < nbHauteur; i++) {
+            this.cases[i] = new Array(nbLargeur);
+        }
+        this.createTable();
         this.update();
     }
-    drawlines(){
-        for (let x = 0; x < this.nbHauteur; x++) {
-            ctx.beginPath();
-            ctx.moveTo(x*canvas.width/this.nbHauteur,0);
-            ctx.lineTo(x*canvas.width/this.nbHauteur,canvas.height);
-            ctx.stroke();
-            ctx.closePath();
-        }
-        for (let y = 0; y < this.nbLargeur; y++) {
-            ctx.beginPath();
-            ctx.moveTo(0,y*canvas.height/this.nbLargeur);
-            ctx.lineTo(canvas.width,y*canvas.height/this.nbLargeur);
-            ctx.stroke();
-            ctx.closePath();
+    createTable(){
+        for (let i = 0; i < this.nbHauteur; i++) {
+            let ligne = document.createElement("tr");
+            for (let j = 0; j < this.nbLargeur; j++) {
+                this.cases[i,j] = new Case(i,j);
+                ligne.appendChild(this.cases[i,j].dom);
+            }
+        document.getElementById("map").appendChild(ligne);
         }
     }
     update(){
@@ -36,30 +28,56 @@ class Terrain{
     }
 }
 
+class Case{
+    constructor(x,y){
+        this.dom = document.createElement("td");
+        this.x = x;
+        this.y = y;
+        this.dom.addEventListener("click",function(){
+            player.update(x,y);
+            this.style.background = "green";
+        });
+    }
+    plante(){
+        this.dom.style.background = "red";
+    }
+} 
+
 class Player{
     constructor(){
-        this.mouseX = 0;
-        this.mouseY = 0;
+        this.selectedX = 0;
+        this.selectedY = 0;
     }
-    update(e){
-        let pos = getMousePos(canvas, e);
-        this.mouseX = pos.x;
-        this.mouseY = pos.y;
-        console.log(this.mouseX);
-        console.log(this.mouseY);
+    update(x,y){
+        console.log(x,y);
+        this.selectedX = x;
+        this.selectedY = y;
     }
-}
-
-function getMousePos(canvas, evt) {
-    var rect = canvas.getBoundingClientRect();
-    return {
-      x: evt.clientX - rect.left,
-      y: evt.clientY - rect.top
-    };
+    planter() {
+        send("planter",JSON.stringify(
+            {
+                "fonction": "planter",
+                "param": {
+                    "x": player.selectedX,
+                    "y": player.selectedY
+                }
+            }
+        )).then(function(response){
+            if(response.ok) {
+                response.json().then(function(json) {
+                    console.log(json);
+                    if(json.reponse == 1){
+                        console.log(player.selectedX,player.selectedY);
+                        terrain.cases[player.selectedX,player.selectedY].plante();
+                    }
+                });
+            }} 
+        )
+    }
 }
 
 function send(task,data) {
-    return fetch("http://localhost:8080/"+task,{
+    return fetch("http://localhost:8081/"+task,{
         method: "POST",
         headers: {
             'Accept': 'application/json, text/plain, */*',
@@ -69,27 +87,10 @@ function send(task,data) {
     })
 }
 
-function planter(x,y) {
-    send("planter",JSON.stringify(
-        {
-            "fonction": "planter",
-            "param": {
-                "x": x,
-                "y": y
-            }
-        }
-    )).then(function(response){
-        if(response.ok) {
-            response.json().then(function(json) {
-                console.log(json);
-                if(json.reponse == 1){
-                    ctx.fillRect(x,y,100,100); 
-                }
-            });
-        }} 
-    )
-}
 
-let terrain = new Terrain(10,10);
+
+let terrain = new Terrain(5,10);
 let player = new Player();
-document.getElementById("canvas").addEventListener("click",player.update);
+document.getElementById('buttonPlanter').addEventListener("click", player.planter);
+
+
