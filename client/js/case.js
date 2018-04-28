420a1414df352eb9bfce0364e7c4e5526c985c85
arnaudgregoire <arnaud.gregoire@ensg.eu>
2018-04-28 18:15:44 +0200

    textures

--- client/js/case.js
+++ client/js/case.js
@@ -1,8 +1,15 @@
+/**
+ * Case est une classe abstraite jamais instancié, elle représente l'ensemble des parcelles du terrains 
+ */
 class Case{
     constructor(x,y){
         this.dom = document.createElement("td");
         this.x = x;
         this.y = y;
+        this.dom.addEventListener("click",function(){
+            player.update(this);
+            this.actionsPossibles();
+        }.bind(this));
     }
     
     actionsPossibles(){
@@ -13,13 +20,12 @@ class Case{
     }
 }
 
+/**
+ * Classe symbolisant l'ensemble des cases vides qui n'appartiennent à personne
+ */
 class EmptyCase extends Case{
     constructor(x,y){
         super(x,y);
-        this.dom.addEventListener("click",function(){
-            player.update(this);
-            this.actionsPossibles();
-        }.bind(this));
     }
 
     actionsPossibles(){
@@ -28,6 +34,27 @@ class EmptyCase extends Case{
     }
     
     achat(){
-        this.dom.style.background = "brown";
+        let bought =  new BoughtCase(player.selectedCase.x,player.selectedCase.y);
+        terrain.cases[player.selectedCase.x][player.selectedCase.y] = bought;
+        this.dom.parentElement.replaceChild(bought.dom,this.dom);
+        bought.actionsPossibles();
+    }
+}
+
+/**
+ * Classe symoblisant l'ensemble des cases qui ont été achetés par un joueur
+ */
+class BoughtCase extends Case{
+    constructor(x,y){
+        super(x,y);
+        this.dom.style.backgroundImage = "url('img/labours.jpg')";
+    }
+
+    actionsPossibles(){
+        super.actionsPossibles();
+        document.getElementById("actions").appendChild(boutonPlanter);
+        document.getElementById("actions").appendChild(boutonRecolter);
+        document.getElementById("actions").appendChild(boutonFertiliser);
+
     }
 }
