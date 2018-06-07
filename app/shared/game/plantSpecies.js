527b6677b40501b8fb17c3845407d64f3d334ae4
arnaudgregoire <arnaud.gregoire@ensg.eu>
2018-06-07 11:39:12 -0400

    PlantSpecies + first Bouquet class

---
+++ app/shared/game/plantSpecies.js
@@ -0,0 +1,29 @@
+(function () {
+    "use strict";
+    let isNode = (typeof module !== 'undefined' && typeof module.exports !== 'undefined');
+
+    class PlantSpecies{
+  
+      static getSpecies(){
+        return ["rose", "tulip"]
+      }
+
+      static getRandomSpecies(){
+        let species = this.getSpecies();
+        return species[Math.floor(Math.random() * species.length)];
+      }
+    }
+
+	// Node export
+	if (isNode) {
+	  module.exports = {
+    PlantSpecies: PlantSpecies
+	  };
+	}
+	// Browser export
+	else {
+	  window.PlantSpecies = PlantSpecies;
+	}    
+
+  })();
+  
