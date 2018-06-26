f8d818b57d89584fa455873556935e913fe9256f
thomas sinno <thomas.sinno@bshg.com>
2018-06-26 15:43:09 +0200

    clear bdd script

---
+++ init_scripts/clearLoginDb.js
@@ -0,0 +1,8 @@
+const pg = require('pg');
+const connectionString = process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost:5432/flowertycoonsimulator';
+console.log(connectionString);
+const client = new pg.Client(connectionString);
+client.connect();
+const result = client.query(
+  'DELETE FROM login');
+
