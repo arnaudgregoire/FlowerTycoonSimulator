98d2534ec1074e42f1a2ef1ddcbe2bd59bbbb31e
ThomasSINNO <thomas.sinno@orange.fr>
2018-08-31 16:32:39 +0200

    update database version init script

--- initLoginDb.js
+++ initLoginDb.js
@@ -1,11 +1,25 @@
 const pg = require('pg');
-const connectionString = process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost:5432/flowertycoonsimulator';
-console.log(connectionString);
-const client = new pg.Client(connectionString);
-client.connect();
-const result = client.query(
-  'CREATE TABLE login(id SERIAL PRIMARY KEY, name VARCHAR(40) not null, active BOOLEAN, password VARCHAR(40) not null)');
-client.end();
+
+const config = {
+	user: 'postgres',
+	host: 'localhost',	
+	password: 'postgres',
+	database: 'flowertycoonsimulator',
+	port: 5432
+		
+}
+
+const pool = new pg.Pool(config)
+
+pool.query('CREATE TABLE login(id SERIAL PRIMARY KEY, name VARCHAR(40) not null, active BOOLEAN, password VARCHAR(40) not null)', (err, res)=>{
+	console.log('Error : ')
+	console.log(err)
+	console.log('\n' + 'Result : ')
+	console.log(res)
+	pool.end()
+});
+
+
 
 
 
