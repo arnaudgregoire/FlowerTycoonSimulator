eaa241cb7c2c25119fa07695109f71ef8e202c34
arnaud.gregoire <arnaud.gregoire@ptvgroup.com>
2019-03-13 09:33:40 +0100

    small refactoring part 2

--- app/server/dbManager.js
+++ app/server/dbManager.js
@@ -4,7 +4,6 @@ var utils      = require('../shared/utils.js');
 
 class DbManager{
     async checkLogin(req){
-        const results = [];
         let username = req.param.player.username;
         let password = req.param.player.password;
         let client = new pg.Client(connectionString);
@@ -18,7 +17,6 @@ class DbManager{
     }
     
     async addUser(req){
-        const results = [];
         let username = req.param.player.username;
         let password = req.param.player.password;
         let client = new pg.Client(connectionString);
@@ -38,21 +36,10 @@ class DbManager{
      * @param {Request body} req
      *  Renvoie un json avec la reponse associe et une petite description de ce qui s est passe
      */
-
-
     async login(req) {
         let json = await this.checkLogin(req).then((results) => {
         if(results.rows[0]){
-            /*
-            console.log("hey");
-            console.log(results.rows[0]);
-            console.log("hi");
-            console.log(results.rows[0].name);
-            console.log(this);
-            */
             let json = {"response": 1, "description" : "Heureux de vous revoir", "player": {"name": results.rows[0].name, "id":results.rows[0].id}};
-
-            //console.log(this);
             return json;
         }else{
             console.log("bad credentials");
@@ -60,8 +47,6 @@ class DbManager{
             return json;
         }
         });
-        //let player = new Player(res.id,res.name,utils.getRandomColor());
-        //this.addNewPlayer(player);
         return json;
     }
     
